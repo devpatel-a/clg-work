1927e3f671866f2b643e59931d8ef95ee7ecc122
Dev Patel <devmikeshpatel@gmail.com>
2025-01-28 23:01:43 +0530

    completed 4 que

---
+++ Q-1.py
@@ -0,0 +1,9 @@
+#Print largest and smallest values out of 2
+a=float(input("Enter 1st number: "))
+b=float(input("Enter 2nd number: "))
+if a>b:
+    print(a,"is largest")
+elif a<b:
+    print(b,"is largest")
+else:
+    print(a,"=",b)
---
+++ Q-3.py
@@ -0,0 +1,6 @@
+#Check odd or even
+a=float(input("Enter a number: "))
+if a%2==0:
+    print(a,"is even")
+else:
+    print(a,"is odd")
